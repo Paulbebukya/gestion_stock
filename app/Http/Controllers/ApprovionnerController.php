1f95b020473d052a9f451bf3fc7b3b15071167f3
paul <wise10paul@gmail.com>
2023-09-11 17:31:33 +0100

    migrations tables

---
+++ app/Http/Controllers/ApprovionnerController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Approvionner;
+use Illuminate\Http\Request;
+
+class ApprovionnerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Approvionner $approvionner)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Approvionner $approvionner)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Approvionner $approvionner)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Approvionner $approvionner)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\ApprovionnerController;
+use App\Http\Controllers\CommanderController;
+use App\Http\Controllers\FournisseurController;
+use App\Http\Controllers\MecanicienController;
+use App\Http\Controllers\PieceController;
+use App\Http\Controllers\ProfileController;
+use App\Models\Fournisseur;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +23,68 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+// crud fournisseur
+Route::controller(FournisseurController::class)->group(function(){
+     route::get('/fournisseur', 'index')->name('fournisseur.index');
+     route::get('/fournisseur/edit', 'edit')->name('fournisseur.edit');
+     route::get('/fournisseur/update', 'update')->name('fournisseur.update');
+     route::get('/fournisseur/destroy/', 'destroy')->name('fournisseur.destroy');
+
+});
+
+//crud mecanicien 
+Route::controller(MecanicienController::class)->group(function(){
+    route::get('/mecanicien', 'index')->name('mecanicien.index');
+    route::get('/mecanicien/edit', 'edit')->name('mecanicien.edit');
+    route::get('/mecanicien/update', 'update')->name('mecanicien.update');
+    route::get('/mecanicien/destroy/', 'destroy')->name('mecanicien.destroy');
+
+});
+
+
+//crud piece
+Route::controller(PieceController::class)->group(function(){
+    route::get('/piece', 'index')->name('piece.index');
+    route::get('/piece/edit', 'edit')->name('piece.edit');
+    route::get('/piece/update', 'update')->name('piece.update');
+    route::get('/piece/destroy/', 'destroy')->name('piece.destroy');
+
+});
+
+
+//crud commander
+Route::controller(CommanderController::class)->group(function(){
+    route::get('/commande', 'index')->name('commande.index');
+    route::get('/commande/edit', 'edit')->name('commande.edit');
+    route::get('/commande/update', 'update')->name('commande.update');
+    route::get('/commande/destroy/', 'destroy')->name('commande.destroy');
+
+});
+
+
+//crud approvionner
+Route::controller(ApprovionnerController::class)->group(function(){
+    route::get('/approvionner', 'index')->name('approvionner.index');
+    route::get('/approvionner/edit', 'edit')->name('approvionner.edit');
+    route::get('/approvionner/update', 'update')->name('approvionner.update');
+    route::get('/approvionner/destroy/', 'destroy')->name('approvionner.destroy');
+});
+
+
+
+
+
+
+require __DIR__.'/auth.php';
